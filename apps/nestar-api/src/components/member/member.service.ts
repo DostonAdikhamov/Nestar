125521b0795b22d047187c3e456e927970706d34
Doston Adikhamov <adikhamov94@gmail.com>
2026-01-01 19:21:32 +0900

    feat: develop global GraphQL error handling system

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -17,16 +17,15 @@ export class MemberService {
         // TODO: Authentication via TOKEN
         return result;
         } catch(err) {
-          console.log("Error, Service.model:", err);
-          throw new BadRequestException(err);
+          console.log("Error, Service.model:", err.message);
+          throw new BadRequestException(Message.USED_MEMBER_NICK_OR_PHONE);
         }
     }
 
     public async login(input: LoginInput): Promise<Member> {
-    
         const { memberNick, memberPassword } = input;
-        const response: Member | null = await this.memberModel.
-          findOne({ memberNick: memberNick })
+        const response: Member = await this.memberModel
+          .findOne({ memberNick: memberNick })
           .select("+memberPassword")
           .exec(); 
 

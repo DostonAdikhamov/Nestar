76f45e9f647811cc846560503e00bde3ed73636f
Doston Adikhamov <adikhamov94@gmail.com>
2026-01-01 18:57:07 +0900

    fix: integrate global pipe validation

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -25,8 +25,8 @@ export class MemberService {
     public async login(input: LoginInput): Promise<Member> {
     
         const { memberNick, memberPassword } = input;
-        const response: Member = await this.memberModel
-          .findOne({ memberNick: memberNick })
+        const response: Member | null = await this.memberModel.
+          findOne({ memberNick: memberNick })
           .select("+memberPassword")
           .exec(); 
 
